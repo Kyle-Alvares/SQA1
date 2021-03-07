ddbafda372dbd5b4701bf90d005525a6942e505b
kyle <kyle.alv2000@gmail.com>
2021-03-07 16:56:53 -0500

    Proper File Extensions

--- tests/withdraw/withdraw1.sh
+++ tests/withdraw/withdraw1.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 make
-cat input/withdraw/withdraw1.txt | ./bank > actual/withdraw/withdraw1.txt
-diff actual/withdraw/withdraw1.txt expected/withdraw/withdraw1.txt
+cat input/withdraw/withdraw1.inp | ./bank > actual/withdraw/withdraw1.atf
+diff actual/withdraw/withdraw1.atf expected/withdraw/withdraw1.etf
